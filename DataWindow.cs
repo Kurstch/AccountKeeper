588e16b0f2a6e9cdcbc8cca069841f2204a1b131
Mārtiņš Griķis <martinsgrikis43@gmail.com>
2020-04-30 13:58:34 +0300

    Add project files.

---
+++ DataWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountKeeper
+{
+    public partial class DataWindow : Form
+    {
+        MenuStrip menuStrip = null;
+
+        public DataWindow()
+        {
+            InitializeComponent();
+            InitializeUiBackground();
+        }
+
+        private void InitializeUiBackground()
+        {
+            this.BackColor = Color.FromArgb(38, 38, 38);
+
+            menuStrip = new MenuStrip();
+            menuStrip.Dock = DockStyle.Top;
+            menuStrip.BackColor = Color.FromArgb(85, 85, 85);
+            this.Controls.Add(menuStrip);
+        }
+    }
+}
